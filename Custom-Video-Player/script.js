fe8c999920fa4cc8e3d686414a2abdc0cfe14b8e
Sarang Wadode <sarangwadode@gmail.com>
2021-12-10 09:47:39 +0530

    video volime and playback speed button added

--- Custom-Video-Player/script.js
+++ Custom-Video-Player/script.js
@@ -2,6 +2,7 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const pausePlay = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
+const sliders = player.querySelectorAll('.player__slider')
 
 function playPause() {
     (video.paused) ? video.play() : video.pause();
@@ -13,11 +14,24 @@ function updateButton() {
 }
 
 function skip() {
-    console.log(video.currentTime)
     video.currentTime += parseInt(this.dataset.skip);
-    console.log(video.currentTime)
 }
+
+function sliding(e) {
+    console.log(e.target.name)
+    if(e.target.name === 'volume') {
+        video.volume = e.target.value;
+    }
+    else {
+        video.playbackRate = (parseFloat(e.target.value) * 16) / 2;
+    }
+    console.log(video.playbackRate)
+}
+
+
 
 video.addEventListener('click', playPause);
 pausePlay.addEventListener('click', playPause);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+sliders.forEach(button => button.addEventListener('change',sliding));
+sliders.forEach(button => button.addEventListener('input',sliding));
