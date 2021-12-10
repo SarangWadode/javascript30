b84412b64a2de1590b0b564f01370f3ec1b91ba5
Sarang Wadode <sarangwadode@gmail.com>
2021-12-10 10:57:37 +0530

    video status bar updated

--- Custom-Video-Player/script.js
+++ Custom-Video-Player/script.js
@@ -2,7 +2,9 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const pausePlay = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const sliders = player.querySelectorAll('.player__slider')
+const sliders = player.querySelectorAll('.player__slider');
+const progress = player.querySelector('.progress');
+const progressFilled = progress.querySelector('.progress_filled');
 
 function playPause() {
     (video.paused) ? video.play() : video.pause();
@@ -25,13 +27,19 @@ function sliding(e) {
     else {
         video.playbackRate = (parseFloat(e.target.value) * 16) / 2;
     }
-    console.log(video.playbackRate)
+}
+
+function videoStatus() {
+    progressFilled.style.width = `${(video.currentTime / video.duration)*100}%`;
 }
 
 
+document.querySelector('[name=playbackRate]').value = video.playbackRate
+document.querySelector('[name=volume]').value = video.volume
 
 video.addEventListener('click', playPause);
 pausePlay.addEventListener('click', playPause);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 sliders.forEach(button => button.addEventListener('change',sliding));
 sliders.forEach(button => button.addEventListener('input',sliding));
+video.ontimeupdate = videoStatus;
