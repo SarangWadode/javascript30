27ea313d4242989241e6df1409cefd22e58899bf
Sarang Wadode <sarangwadode@gmail.com>
2021-12-14 10:42:56 +0530

    final commit

--- mouse-move-shadow/script.js
+++ mouse-move-shadow/script.js
@@ -2,10 +2,13 @@ const hero = document.querySelector('.hero');
 
 function moveShadow(e) {
     const head = this.querySelector('h1');
+    const width = hero.offsetWidth;
+    const height = hero.offsetHeight;
     const x = e.clientX;
     const y = e.clientY;
-    head.style.textShadow = `${x/10}px ${y/10}px 0 rgb(156, 55, 55)`;
-    console.log(e.clientX)
-    // console.log(head.style.textShadow)
+    const walkX = Math.round(x/width*100 - 50);
+    const walkY = Math.round(y/height*100 - 50);
+    console.log(walkX,walkY)
+    head.style.textShadow = `${walkX}px ${walkY}px 0 rgb(55, 153, 156)`
 }
 hero.addEventListener('mousemove',moveShadow);
