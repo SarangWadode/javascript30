6120326931ed975deb44bf26e77b65fae5ec68dc
Sarang Wadode <sarangwadode@gmail.com>
2021-12-14 16:49:38 +0530

    list sorted by ignoring articles

--- sort-without-articles/script.js
+++ sort-without-articles/script.js
@@ -1,1 +1,11 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+bands.sort((a,b) => (strip(a) > strip(b) ? 1:-1))
+function strip(bandName) {
+    return bandName.replace(/^(a|an|the)\s/i,'').trim();
+}
+let list = '';
+document.querySelector('#bands').innerHTML = bands.map(band => list += `<li>${band}</li>`).join('');
+// bandList.innerHTML = bands.reduce(
+//     (s, band) => s + `<li>${band}</li>`, 
+//     '');
+// console.log(list)
