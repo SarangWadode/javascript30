0d84a71b423405ad114b956431b9fecc6d5c40b3
Sarang Wadode <sarangwadode@gmail.com>
2021-12-15 18:00:04 +0530

    with using reduce

--- adding-up-times-with-reduce/script.js
+++ adding-up-times-with-reduce/script.js
@@ -1,13 +1,26 @@
 const lists = document.querySelectorAll('[data-time]');
-let count = [];
-let mins = 0 ;
-let secs = 0 ;
-lists.forEach(list => {
-    let time = list.dataset.time.split(':');
-    mins += parseInt(time[0]);
-    secs += parseInt(time[1]);
-});
-let totalSeconds = mins * 60 + secs;
+// Without using .reduce()
+
+// let count = [];
+// let mins = 0 ;
+// let secs = 0 ;
+// lists.forEach(list => {
+//     let time = list.dataset.time.split(':');
+//     mins += parseInt(time[0]);
+//     secs += parseInt(time[1]);
+// });
+// let totalSeconds = mins * 60 + secs;
+
+// using .reduce()
+const nodeList = [...lists];
+const seconds = nodeList
+    .map(list => list.dataset.time)
+    .map(timeCode => {
+        const [mins , secs] = timeCode.split(':').map(parseFloat)
+        return (mins * 60) + secs;})
+    .reduce((total,secs) => total + secs);
+
+let totalSeconds = seconds;
 let hrs = Math.floor(totalSeconds / 3600); 
 totalSeconds = totalSeconds % 3600;
 let minsleft = Math.floor(totalSeconds / 60);
