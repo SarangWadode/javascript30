22be6ed15f03547ae3c52b0f0b1943ee85df3a7c
Sarang Wadode <sarangwadode@gmail.com>
2021-12-18 17:52:26 +0530

    image filters added

--- webcam-fun/script.js
+++ webcam-fun/script.js
@@ -23,7 +23,18 @@ function paintToCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video,0,0,width,height);
-    },15)
+        //get image data
+        let pixels = ctx.getImageData(0,0,width,height);
+        //change pixels data
+        pixels = redEffect(pixels);
+
+        pixels = rgbSplit(pixels);
+        ctx.globalAlpha = 0.8;
+        pixels = greenScreen(pixels);
+        //put back the pixels data
+        ctx.putImageData(pixels,0,0);
+
+    },15);
 }
 
 function snapPhoto() {
@@ -34,9 +45,52 @@ function snapPhoto() {
     const link = document.createElement('a');
     link.href = data;
     link.setAttribute('download','handsomeboy')
-    link.textContent = "Download Image"
+    link.innerHTML = `<img src=${data} alt="Cute boy">`;
     strip.insertBefore(link,strip.firstChild);
 }
+
+function redEffect(pixels) {
+    for (let i = 0; i <= pixels.data.length; i += 4) {
+        pixels.data[i+0] = pixels.data[i+0]; 
+        pixels.data[i+1] = pixels.data[i+1];
+        pixels.data[i+2] = pixels.data[i+2];
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i <= pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i+0] 
+        pixels.data[i + 100] = pixels.data[i+1] 
+        pixels.data[i + 150 ] = pixels.data[i+2]
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+  
+    document.querySelectorAll('.rgb input').forEach((input) => {
+      levels[input.name] = input.value;
+    });
+  
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+      red = pixels.data[i + 0];
+      green = pixels.data[i + 1];
+      blue = pixels.data[i + 2];
+      alpha = pixels.data[i + 3];
+  
+      if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+        pixels.data[i + 3] = 0;
+      }
+    }
+    return pixels;
+  }
 
 video.addEventListener('canplay',paintToCanvas);
 
