e35d17cdb46e09d13a3a8430a2d7fa2db6f33704
Sarang Wadode <sarangwadode@gmail.com>
2021-12-19 01:32:40 +0530

    error resolving

--- geolocation/script.js
+++ geolocation/script.js
@@ -1,10 +1,12 @@
 const arrow = document.querySelector('.arrow');
 const speed = document.querySelector('.speed-value');
-
+const test = document.querySelector('.sample')
 navigator.geolocation.watchPosition(data => {
     console.log(data.coords)
     speed.textContent = data.coords.speed;
     arrow.style.transform = `rotate(${data.coords.heading})deg`;
+    test.innerHTML += `<br> <p>Speed: ${data.coords.speed} Heading: ${data.coords.heading}</p>` 
 }, (err) => {
     console.log("Please allow location to use this Compass and Speedometer :|")
 })
+
