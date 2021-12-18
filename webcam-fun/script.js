7ccc4ec3122fa660b4a807e0fe94bd0c409bddac
Sarang Wadode <sarangwadode@gmail.com>
2021-12-18 12:34:13 +0530

    web cam enabled

--- webcam-fun/script.js
+++ webcam-fun/script.js
@@ -3,3 +3,15 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    const mediaStream = new MediaStream();
+    console.log(mediaStream);
+    video.src = mediaStream;
+    // navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    // .then(localMediaStream => {
+    //     const MediaStream
+    //     video.src = window.URL.createObjectURL(localMediaStream);
+    // })
+}
+getVideo();
